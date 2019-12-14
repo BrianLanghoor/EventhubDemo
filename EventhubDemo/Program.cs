d03309624e16345a5f727f287a30979764950e8c
Brian Langhoor <brian.langhoor@infocaster.net>
2019-12-14 23:21:26 +0100

    Moved stuf around

--- EventhubDemo/Program.cs
+++ EventhubDemo/Program.cs
@@ -7,21 +7,38 @@ using Microsoft.Azure.EventHubs.Processor;
 
 namespace EventhubDemo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //SendMessageToEventHubs();
-            ReceiveMessages(args).GetAwaiter().GetResult();
+            SendMessageToEventHubs();
+            //ReceiveMessages(args).GetAwaiter().GetResult();
+        }
+
+        private static void SendMessageToEventHubs()
+        {
+            const string sendEventHubConnectionString = "<<<Sender Event Hub ConnectionString>>>";
+
+            var ehClient = EventHubClient.CreateFromConnectionString(sendEventHubConnectionString);
+
+            for (var i = 0; i < 100; i++)
+            {
+                var eventData = new EventData(Encoding.UTF8.GetBytes($"My awesome message #{i}"));
+                ehClient.SendAsync(eventData);
+                Console.WriteLine($"Message #{i} sent");
+                Thread.Sleep(50);
+            }
+
+            Console.WriteLine("Messages sent");
         }
 
         private static async Task ReceiveMessages(string[] args)
         {
-            const string listenEventHubConnectionString = "Endpoint=sb://brianexperiment.servicebus.windows.net/;SharedAccessKeyName=ListenOnly;SharedAccessKey=YZEenPYwQBrky0ks6BMCaEFQYeszfXeet4qc8bHUDOQ=";
-            const string eventHubName = "brianeventhub";
-            const string storageContainerName = "eventhubcontainer";
-            const string storageAccountName = "brianblobeventhubs";
-            const string storageAccountKey = "RxmlN8ZSD1AgbW+Jwq1n9UdCPjHVLMI5PoishBrm28EdLkP203A5bn0MQ90Hi1WehDE3B/CuZj7UIZKLgU7aXQ==";
+            const string listenEventHubConnectionString = "<<<Listen Event Hub ConnectionString>>>";
+            const string eventHubName = "<<<Event Hub Name>>>";
+            const string storageContainerName = "<<<Storage Container Name>>>";
+            const string storageAccountName = "<<<Storage Account Name>>>";
+            const string storageAccountKey = "<<<StorageAccountKey>>>";
 
             var storageConnectionString = $"DefaultEndpointsProtocol=https;AccountName={storageAccountName};AccountKey={storageAccountKey}";
 
@@ -34,30 +51,14 @@ namespace EventhubDemo
                 storageConnectionString,
                 storageContainerName);
 
+            var processorOptions = new EventProcessorOptions{};
             // Registers the Event Processor Host and starts receiving messages
-            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(new EventProcessorOptions());
 
             Console.WriteLine("Receiving.");
 
             // Disposes of the Event Processor Host
             await eventProcessorHost.UnregisterEventProcessorAsync();
-        }
-
-        private static void SendMessageToEventHubs()
-        {
-            const string sendEventHubConnectionString = "Endpoint=sb://brianexperiment.servicebus.windows.net/;SharedAccessKeyName=SendOnlyPolicy;SharedAccessKey=jK+jJuxyEk/mZqO1YvbcsnPAWOvHwLbW5qSOMbsUWbo=;EntityPath=brianeventhub";
-
-            var ehClient = EventHubClient.CreateFromConnectionString(sendEventHubConnectionString);
-
-            for (int i = 0; i < 100; i++)
-            {
-                var eventData = new EventData(Encoding.UTF8.GetBytes($"My awesome message #{i}"));
-                ehClient.SendAsync(eventData);
-                Console.WriteLine($"Message #{i} sent");
-                Thread.Sleep(50);
-            }
-
-            Console.WriteLine("Messages sent");
         }
     }
 }
